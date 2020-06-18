22680812176ccdea3f604712595c14a5539d2d88
Tim Bardouille <tim.bardouille@dal.ca>
2020-06-18 17:08:46 -0300

    MODIFIED: output directory for dipole files

--- scripts/simulate_over_range.py
+++ scripts/simulate_over_range.py
@@ -9,19 +9,12 @@ Adapted from:
 
 '''
 
-startingParameterFile = 'AlphaAndBeta_testScript.param'
-paramOfInterest = 'input_prox_A_weight_L2Pyr_ampa'
-paramMin = 0.1e-5
-paramMax = 9.4e-5
-numberOfSteps = 10
-parameterScale = 'linear'
-
-plotOK = True
-
 ###############################################################################
 # Imports and setup
 
 import os.path as op
+import os, shutil
+
 import numpy as np
 import matplotlib.pyplot as plt
 import multiprocessing as mp
@@ -32,44 +25,60 @@ from hnn_core import simulate_dipole, read_params, read_dipole, Network
 # Directories and Neuron
 hnn_core_root = op.join(op.dirname(hnn_core.__file__), '..')
 
+
+###############################################################################
+# User-defined variables
+startingParameterFile = 'AlphaAndBeta_testScript.param'
+paramsOfInterest = ['input_prox_A_weight_L2Pyr_ampa', 
+	'input_prox_A_weight_L5Pyr_ampa',
+	'input_dist_A_weight_L2Pyr_ampa',
+	'input_dist_A_weight_L5Pyr_ampa']
+paramMin = 0.1e-5
+paramMax = 9.4e-5
+numberOfSteps = 10
+parameterScale = 'linear'
+
+dplDirName = 'pyr_ampa'
+
+plotOK = True
+
 # Folder for storing dipole txt files
 outDir = '/Users/tbardouille/Documents/Work/Projects/Spectral Events/HNN/Data'
 
-
 ###############################################################################
 # Functions
-def adjust_param_and_simulate_dipole_mp(thisParams, paramOfInterest, paramValue, outDir):
+def adjust_param_and_simulate_dipole(thisParams, paramsOfInterest, paramValue, outDir):
 
-	thisParams.update({paramOfInterest: paramValue})
+	for paramOfInterest in paramsOfInterest:
+		thisParams.update({paramOfInterest: paramValue})
 
 	print(thisParams)
 
 	net = Network(thisParams)
 	dpl = simulate_dipole(net)
 
-	out_fname = op.join(outDir, "{0:.5e}.txt".format(paramValue))
-
 	return dpl
 
 def adjust_param_and_simulate_dipole_mp(paramValue):
 
-	params.update({paramOfInterest: paramValue})
+	for paramOfInterest in paramsOfInterest:
+		params.update({paramOfInterest: paramValue})
 
 	net = Network(params)
 	dpl = simulate_dipole(net)
 
-	out_fname = op.join(outDir, "{0:.5e}.txt".format(paramValue))
+	out_fname = op.join(dplPath, "{0:.5e}.txt".format(paramValue))
 
 	dpl[0].write(out_fname)
 
 	return dpl
 
-def read_dipoles_over_range(parameterValues, outDir, plotOK):
+def read_dipoles_over_range(parameterValues, plotOK):
 	
 	# Read simulations
 	dpls = []
 	for p in parameterValues:
-		fname = op.join(outDir, "{0:.5e}.txt".format(p))
+		fname = op.join(dplPath, "{0:.5e}.txt".format(p))
 		dpls.append(read_dipole(fname))
 
 	if plotOK:
@@ -81,7 +90,7 @@ def read_dipoles_over_range(parameterValues, outDir, plotOK):
 		plt.plot(dpls[0].t, dipAmp)
 		plt.xlim(50, 700)
 		plt.ylim(-500,500)
-		plt.xlabel('Time [s]')
+		plt.xlabel('Time [ms]')
 		plt.ylabel('Dipole Strength [nAm]')
 		plt.legend(parameterValues)
 		plt.show()
@@ -117,25 +126,32 @@ if parameterScale == 'linear':
 if parameterScale == 'log':
 	parameterValues = np.logspace(paramMin, paramMax, numberOfSteps, endpoint=True)
 
-
 '''
-# Run simulations - Loop over parameter range and generate dipole simulations
+# Run simulations consecutively
+# Loop over parameter range and generate dipole simulations
 dpls = []
 for p in parameterValues:
-	dpls.append(adjust_param_and_simulate_dipole(params, paramOfInterest, p, outDir))
+	dpls.append(adjust_param_and_simulate_dipole(params, paramsOfInterest, p, outDir))
 '''
 
-# Run simulations
+# Run simulations in parallel
+# Make the folder for the dpl files
+dplPath = op.join(outDir, dplDirName)
+if not os.path.exists(dplPath):
+    os.makedirs(dplPath)
+else:
+	# Remove all the subdirectories first
+    shutil.rmtree(dplPath)           
+    os.makedirs(dplPath)
 # Set up the parallel task pool for dipole simulations
-count = int(np.round(mp.cpu_count()*1/3))
+count = int(np.round(mp.cpu_count()*1/2))
 print(count)
 pool = mp.Pool(processes=count)
 # Run the jobs
 pool.map(adjust_param_and_simulate_dipole_mp, parameterValues)
 
-
 # Read simulated dipole timecourses for all parameter values
-dpls = read_dipoles_over_range(parameterValues, outDir, plotOK)
+dpls = read_dipoles_over_range(parameterValues, plotOK)
 
 # Read smoother dipole amplitude ranges for parameters
 ranges = get_smoothed_ranges(parameterValues[0:-1], dpls[0:-1])
